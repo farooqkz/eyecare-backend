b1d384319a990a6a2de7bd7b3f7764d1aa94fa34
Farooq Karimi Zadeh <fkz@riseup.net>
2024-08-23 16:01:24 +0330

    done with computer vision part

--- computer_vision.py
+++ computer_vision.py
@@ -1,10 +1,12 @@
 import cv2 as cv
-import numpy as np
+import math
+from numpy import ndarray
 from typing import Optional
 
 Circle = tuple[float, float, float]
+Iris = tuple[ndarray, Circle, Circle]
 
-def get_iris(image: numpy.ndarray) -> Optional[tuple[numpy.ndarray, tuple[Circle, Circle]]]:
+def get_iris(image: ndarray) -> Optional[Iris]:
     min_radius = 480 * 0.8 * 0.9
     max_radius = 480 * 0.8 * 1.1
     irides = cv.HoughCircles(
@@ -35,11 +37,28 @@ def get_iris(image: numpy.ndarray) -> Optional[tuple[numpy.ndarray, tuple[Circle
         maxRadius = max_radius_p
     )
  
-    selected_iris = None
-    selected_pupil = None
+    selected_iris: Optional[Circle] = None
+    selected_pupil: Optional[Circle] = None
     for iris_x, iris_y, iris_r in irides[0]:
         for pupil_x, pupil_y, pupil_r in pupils[0]:
             if math.sqrt((pupil_x - iris_x)**2 + (pupil_y - iris_y)**2) <= 10:
                 selected_pupil = (pupil_x, pupil_y, pupil_r)
                 selected_iris = (iris_x, iris_y, iris_r)
                 break
+
+    if selected_iris is None or selected_pupil is None:
+        return None
+    
+    center_x = selected_iris[0]
+    center_y = selected_iris[1]
+    length = selected_iris[2]
+    x_upper = max(image.shape[0], center_x + length)
+    y_upper = max(image.shape[1], center_y + length)
+    x_lower = min(0, center_x - length)
+    y_lower = min(0, center_y - length)
+    image = image[
+        y_lower : y_upper,
+        x_lower : x_upper
+    ]
+
+    return (image, selected_iris, selected_pupil)
